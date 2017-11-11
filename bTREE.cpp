a4766fc99e60dddacad8a6c7d16004ccd8d6ca44
Robert Randolph <rrandol3@uwyo.edu>
2017-11-11 13:55:14 -0700

    Created code for numberofnodes, find, constructor/destructor, destroy (helper function for destructor), and operator<<.

--- bTREE.cpp
+++ bTREE.cpp
@@ -1,43 +1,124 @@
-#include "bTREE.h"
+// bTREE.cpp
+// Robert Randolph & Mckade U.
+// COSC 2030, Section 01
+// Merkle_Votes_HW
+// December 01, 2017
 
+#include "bTREE.h"	// bTREE
 
-//look at descriptions in pMT.h for guidance on what you might need for these function to actually do
+// ===Public Function Definitions===
+
 bTREE::bTREE()
 {
+	tree = NULL;
 }
 
 bTREE::~bTREE()
 {
+	destroy(tree);
 }
 
 int bTREE::dataInserted()
 {
+	return 0;
 }
 
+// Returns the number of nodes in the tree.
 int bTREE::numberOfNodes()
 {
+	return numberOfNodes(tree);
 }
 
 int bTREE::insert(string data, int time)
 {
+	return 0;
 }
 
-int bTREE::find(string)
+/*	Find whether a node exists in the tree.
+	Returns the number of operations to find the node. */
+int bTREE::find(string idata)
 {
+	bool found(false);
+	return find(idata, tree, found, true);
 }
 
-string bTREE::locate()
+string bTREE::locate(string idata)
 {
+	return "";
 }
 
-friend bool bTREE::operator ==(const bTREE& lhs, const bTREE& rhs)
+bool operator ==(const bTREE& lhs, const bTREE& rhs)
 {
+	bool result(false);
+
+	return result;
 }
 
-friend bool bTREE::operator !=(const bTREE& lhs, const bTREE& rhs)
+bool operator !=(const bTREE& lhs, const bTREE& rhs)
 {
+	return false;
 }
 
-friend std::ostream& bTREE::operator <<(std::ostream& out, const bTREE& p)
+std::ostream& operator <<(std::ostream& out, const bTREE& itree)
 {
+	if (itree.tree != NULL) {
+		out << itree.tree;
+	}
+
+	return out;
+}
+
+// ===Private Function Definitions===
+
+/*	Helper function to find number of nodes.
+	Increases size by one as long as the subtree treeNode != NULL */
+int bTREE::numberOfNodes(const treeNode * subtree)
+{
+	int size(0);
+
+	if (subtree != NULL) {
+		size++;
+		size += numberOfNodes(subtree->left);
+		size += numberOfNodes(subtree->right);
+	}
+
+	return size;
+}
+
+/*	Helper function to find whether a node exists in the tree.
+	Returns the number of operations to find the node.
+	Returns 0 if the node doesn't exist. */
+int bTREE::find(const string idata, const treeNode * subtree, bool & found, bool root)
+{
+	int operations(0);
+
+	if (!found && subtree != NULL) {
+		operations++;
+		if (idata != subtree->data) {
+			operations += find(idata, subtree->left, found, false);
+			operations += find(idata, subtree->right, found, false);
+		}
+		else found = true;	// Found that the node exists.
+
+		// Checking if at the root node and whether or not the node was found.
+		if (!found && root) {
+			operations = 0;
+		}
+	}
+
+	return operations;
+}
+
+/*	Helper function for destructor.
+	Destroys all nodes in the tree. */
+void bTREE::destroy(treeNode * subtree)
+{
+	if (subtree != NULL) {
+		destroy(subtree->left);
+		destroy(subtree->right);
+		delete subtree;
+		subtree = NULL;
+	}
+
+	return;
 }
--- bTREE.h
+++ bTREE.h
@@ -2,18 +2,20 @@
 // Robert Randolph & Mckade U.
 // COSC 2030, Section 01
 // Merkle_Votes_HW
-// November 10, 2017
+// December 01, 2017
 
 #ifndef BTREE_H
 #define BTREE_H
 
 #include <string>	// string
+#include <vector>	// vector
 
 using std::string;
+using std::vector;
 
 class bTREE
 {
-    struct treeNode{
+    struct treeNode {
 		treeNode(int itime = 0, string idata = "Data", bool ileaf = false,
 			treeNode *ileft = NULL, treeNode *iright = NULL)
 			: time(itime), data(idata), leaf(ileaf), left(ileft), right(iright) {}
@@ -24,23 +26,49 @@ class bTREE
 
 		treeNode *left;
 		treeNode *right;
+
+		friend std::ostream& operator <<(std::ostream& out, const treeNode * subtree)
+		{
+			if (subtree != NULL) {
+				out << subtree->left;
+				out << "Time: " << subtree->time
+					<< " :: Vote: " << subtree->data << '\n';
+				out << subtree->right;
+			}
+
+			return out;
+		}
     };
     
 private:
-    //some data structure to hold your treeNodes together ...
-    //DATASTUCTURE treeNodes tree;
-    //any helper private variables you need
+	treeNode *tree;
+
+/*	Helper function to find number of nodes.
+	Returns the number of nodes in the tree. */
+	static int numberOfNodes(const treeNode *);
+
+/*	Helper function to find wether a node exists in the tree.
+	Returns the number of operations to find the node. */
+	static int find(const string, const treeNode *, bool &, bool);
+
+/*	Helper function for destructor.
+	Destroys all nodes in the tree. */
+	static void destroy(treeNode *);
     
 public:
     bTREE();
     ~bTREE();
     
     int dataInserted();
+
+	// Returns the number of nodes in the tree.
     int numberOfNodes();
     
-    bool insert(string, int);
+    int insert(string, int);
     
-    bool find(string);
+/*	Find whether a node exists in the tree.
+	Returns the number of operations to find the node. */
+    int find(string);
     
     string locate(string);
     
@@ -48,7 +76,7 @@ public:
     friend bool operator==(const bTREE& lhs, const bTREE& rhs);
     friend bool operator!=(const bTREE& lhs, const bTREE& rhs);
 
-    friend std::ostream& operator<<(std::ostream& out, const bTREE& p);
+    friend std::ostream& operator<<(std::ostream& out, const bTREE&);
     
 };
 
